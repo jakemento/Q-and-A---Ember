72d151f1334721c7575902374308b555946a597a
Jake Steinberg <jakesteinberg@gmail.com>
2016-04-01 09:16:31 -0700

    add dynamic routing for each question page

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,24 +6,9 @@ export default Ember.Route.extend({
   },
 
   actions: {
-    deleteQuestion(question) {
-      question.destroyRecord();
-      this.transitionTo('index');
-    },
-
     save3(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
-      this.transitionTo('index');
-    },
-
-    update(question, params) {
-      Object.keys(params).forEach(function(key) {
-        if(params[key]!==undefined) {
-          question.set(key,params[key]);
-        }
-      });
-      question.save();
       this.transitionTo('index');
     },
   }

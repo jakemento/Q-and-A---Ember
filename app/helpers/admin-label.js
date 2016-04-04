5d2deda5836d7f6015de1d1f26c05c244753626b
Jake Steinberg <jakesteinberg@gmail.com>
2016-04-03 17:11:59 -0700

    add a custom helper for showing which questions were posted by the admin

---
+++ app/helpers/admin-label.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export function adminLabel(params/*, hash*/) {
+  var author = params[0].get('author');
+
+  if (author == "Jake S.") {
+    return 'ADMIN'
+  }
+}
+
+export default Ember.Helper.helper(adminLabel);
---
+++ tests/unit/helpers/admin-label-test.js
@@ -0,0 +1,10 @@
+import { adminLabel } from 'q-and-a/helpers/admin-label';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | admin label');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = adminLabel([42]);
+  assert.ok(result);
+});

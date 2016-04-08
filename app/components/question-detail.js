377405ea279a56c49f4a25bb7562978b1c354031
Jake Steinberg <jakesteinberg@gmail.com>
2016-04-08 12:52:33 -0700

    add ability to add to favorite questions from the  specific question as well as the full list

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
   actions: {
     delete(question) {
       if(confirm('Are you sure you want to delete this Q?')) {
         this.sendAction('deleteQuestion', question);
       }
+    },
+    addToFavorites(question) {
+      this.get('favoriteQuestions').add(question);
     }
   }
 });
